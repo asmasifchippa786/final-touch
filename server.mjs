418b02291d5de789a33aeb7ca9524aa010c5a947
asmasifchippa786 <asmasifchippa786@gmail.com>
2022-12-21 13:25:22 +0500

    1st

--- server.mjs
+++ server.mjs
@@ -1,14 +1,12 @@
-import express from 'express'
-
+import express from 'express';
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-
-    console.log('request ip: ' , req.ip)
-  res.send('Hello World!' , new Date().toString())
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
-
+  
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
