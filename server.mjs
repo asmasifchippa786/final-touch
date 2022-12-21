863a8036b6ebd755ecb53f2e3eee877abd55cdb7
asmasifchippa786 <asmasifchippa786@gmail.com>
2022-12-21 18:19:54 +0500

    2nd

--- server.mjs
+++ server.mjs
@@ -1,12 +1,68 @@
 import express from 'express';
-const app = express()
-const port = process.env.PORT || 3000;
+import path from 'path';
+import cors from 'cors';
 
-app.get('/', (req, res) => {
+const app = express()
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/abc', (req, res) => {
     console.log("request ip: ", req.ip);
     res.send('Hello World! ' + new Date().toString());
 })
-  
+
+//Url params:
+//http://localhost:5001/weather/karachi/75600
+// sequence did care
+//app.get('/weather/:cityName/:zip', (req, res) => {
+
+//query parameters:
+// sequence didnot care
+//http://localhost:5001/weather?cityName=karachi&zip=75600
+
+// app.get('/weather' , (req, res) => {
+// console.log(req.query.cityName)
+// console.log(req.query.zip)
+
+//we can also merge url and query param
+
+// body:
+// http://localhost:5001/weather
+//{
+//    cityName : "Karachi",
+//    zip : 75600
+//}
+
+app.get('/weather/:cityName', (req, res) => {
+    console.log("request ip: ", req.ip);
+    console.log('param: ' , req.params.cityName)
+
+
+
+    res.send({
+        city : req.params.cityName,
+        temp: 30,
+        min: 26,
+        max: 31,
+        humidity: 20,
+        serverTime: new Date().toString(),
+        wind : 8
+    });
+})
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, '/web/build')))
+app.use('*', express.static(path.join(__dirname, '/web/build')))
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
